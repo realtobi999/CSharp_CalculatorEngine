f4a75b1c46b181c646824bf42ca70e64474ab26b
realtobi999 <tobiasfilgas@gmail.com>
2024-10-05 11:21:29 +0200

    [built] added support for subtraction

--- src/Calculator.Core/CalculatorEngine.cs
+++ src/Calculator.Core/CalculatorEngine.cs
@@ -1,3 +1,5 @@
+using System.Net.Security;
+
 namespace Calculator.Core;
 
 public class CalculatorEngine
@@ -6,6 +8,7 @@ public class CalculatorEngine
     {
         var tokens = new List<string>();
 
+        // convert the string like "33+33" into a list like ["33", "+", "33"]
         var num1 = "";
         foreach (var ch in expression)
         {
@@ -17,37 +20,53 @@ public class CalculatorEngine
             {
                 if (num1 != "")
                 {
-                    // add the number and reset
                     tokens.Add(num1);
                     num1 = "";
                 }
-                // add operator
                 tokens.Add(ch.ToString());
             }
         }
+        // add the last number if it exists
+        if (num1 != "") tokens.Add(num1);
 
-        // add the last number if exists
-        if (num1 != "") tokens.Add(num1); 
+        // adjust the tokens for easier usage => attach the "-" symbol directly to the following number
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            if (tokens[i] == "-" && int.TryParse(tokens[i + 1], out _))
+            {
+                tokens[i + 1] = "-" + tokens[i + 1];
 
-        // iterate through the first element of the tokens and sum up all the elements until there is only one number left
-        for (int i = 0; i < tokens.Count;) 
+                if (i == 0)
+                {
+                    tokens.RemoveAt(i);
+                }
+                else
+                {
+                    tokens[i] = "+";
+                }
+            }
+        }
+
+        // iterate through the tokens and apply operators until only one number remains
+        while (true)
         {
             if (tokens.Count == 1) break;
-            
-            var token = tokens[i];
+
+            var token = tokens[0];
 
             if (int.TryParse(token, out int num2))
             {
-                if (i + 2 < tokens.Count && tokens[i + 1] == "+" && int.TryParse(tokens[i + 2], out int num3))
+                if (tokens[1] == "+" && int.TryParse(tokens[2], out int num3))
                 {
-                    tokens[i] = (num2 + num3).ToString();
+                    tokens[0] = (num2 + num3).ToString();
 
-                    tokens.RemoveAt(i + 2);
-                    tokens.RemoveAt(i + 1);
+                    tokens.RemoveAt(2);
+                    tokens.RemoveAt(1);
                 }
             }
         }
 
         return int.Parse(tokens.First());
     }
+
 }
--- src/Calculator.Tests/Engine/BasicArithmeticsTests.cs
+++ src/Calculator.Tests/Engine/BasicArithmeticsTests.cs
@@ -16,4 +16,29 @@ public class BasicArithmeticsTests
         engine.Solve("3+3+3").Should().Be(9);
         engine.Solve("33+33+22").Should().Be(88);
     }
+
+    [Fact]
+    public void SubtractionShouldWork()
+    {
+        // prepare
+        var engine = new CalculatorEngine();
+
+        // act & assert
+        engine.Solve("9-3").Should().Be(6);
+        engine.Solve("-3-9").Should().Be(-12);
+        engine.Solve("3-9").Should().Be(-6);
+        engine.Solve("9-3-3-3").Should().Be(0);
+        engine.Solve("90-30-30-30").Should().Be(0);
+    }
+
+    [Fact]
+    public void SubtractionAndAdditionShouldWorkTogether()
+    {
+        // prepare
+        var engine = new CalculatorEngine();
+
+        // act & assert
+        engine.Solve("90+30-90+30").Should().Be(60);
+        engine.Solve("22-0-50+30").Should().Be(2);
+    }
 }
